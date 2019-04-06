137b009f107ac03c0cd814a87696f1600a914ad0
ABuntsev <andrew.buntsev@gmail.com>
2019-04-06 15:10:24 +1100

    Updates

---
+++ precache-manifest.7a4ff344cd25f99eed46d97850f08b4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d8194bde761299ff809",
+    "url": "/pomodoro/static/js/runtime~main.5f1f8454.js"
+  },
+  {
+    "revision": "d78930b83f7dff0124c4",
+    "url": "/pomodoro/static/js/main.b38e611e.chunk.js"
+  },
+  {
+    "revision": "577a1554a3533605473b",
+    "url": "/pomodoro/static/js/2.5205a7ef.chunk.js"
+  },
+  {
+    "revision": "d78930b83f7dff0124c4",
+    "url": "/pomodoro/static/css/main.88ce50cd.chunk.css"
+  },
+  {
+    "revision": "1d887e49675f389ca1e4d7466612708a",
+    "url": "/pomodoro/index.html"
+  }
+];
